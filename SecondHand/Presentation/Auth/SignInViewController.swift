5e76a72b1a9970e860a68b5c98baebb8e9444685
raden-dimas012 <dimascode012@gmail.com>
2022-06-14 18:47:14 +0700

    init structure project and init sign in & sign up after this move to branch feature for development.

---
+++ SecondHand/Presentation/Auth/SignInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignInViewController.swift
+//  SecondHand
+//
+//  Created by Raden Dimas on 14/06/22.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SecondHand/Presentation/Auth/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  SecondHand
+//
+//  Created by Raden Dimas on 14/06/22.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
